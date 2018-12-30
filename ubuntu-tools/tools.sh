9553ade0621ed8295315a1810aec2da19ff4c199
Andrés Montoya <andresmontoyafcb@gmail.com>
2018-12-30 11:36:41 -0500

    Update tools.sh

--- ubuntu-tools/tools.sh
+++ ubuntu-tools/tools.sh
@@ -43,7 +43,7 @@ sudo apt install apt-transport-https ca-certificates software-properties-common 
 
 sleep 10
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 
 sleep 10
@@ -61,8 +61,8 @@ sudo curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
 sudo apt install -y nodejs
 
 # Installing yarn
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+sudo echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update
 sudo apt install yarn -y
 
