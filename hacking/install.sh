8d53eb6280478c828a000cc18cec6e03995b12ee
Andrés Montoya <andresmontoyafcb@gmail.com>
2019-04-19 20:53:41 -0500

    Update install.sh

--- hacking/install.sh
+++ hacking/install.sh
@@ -1,33 +1,28 @@
 # You need to have Docker and git installed before anything!
-
 sudo apt install curl python python3 python3-pip nmap
 
 # Installing bettercap over Docker
 sudo docker pull bettercap/bettercap
-
-echo "Now, with 'docker run -it --privileged --net=host bettercap/bettercap -h' you can start!"
+echo "Now, with 'docker run -it --privileged --network=host bettercap/bettercap -h' you can start!"
 
 # Installing metasploit over Docker
-
 sudo docker pull metasploitframework/metasploit-framework
-
-echo "Now, with 'docker run -it --privileged --net=host metasploitframework/metasploit-framework' you can start!"
+echo "Now, with 'docker run -it --privileged --network=host metasploitframework/metasploit-framework bash' you can start!"
 
 # Installing empire over Docker
-
 sudo docker pull empireproject/empire
+echo "Now, with 'docker run -it --privileged --network=host empireproject/empire bash' you can start!"
 
-echo "Now, with 'docker run -it --privileged --net=host empireproject/empire' you can start!"
+# Installing wifirock over Docker
+sudo docker pull andresmontoyain/wifirock
+echo "Now, with 'docker run -it --rm -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:ro --privileged --network=host --name wifirock andresmontoyain/wifirock bash' you can start!"
 
 # Cloning unicorn
-
 git clone https://github.com/trustedsec/unicorn.git
 
 # Cloning SQLMap
-
 git clone https://github.com/sqlmapproject/sqlmap.git
 
-# Downloading ZAProxy
-
-curl https://github.com/zaproxy/zaproxy/releases/download/2.7.0/ZAP_2.7.0_Linux.tar.gz > ZAP_2.7.0_Linux.tar.gz && \
-  tar -xzvf ZAP_2.7.0_Linux.tar.gz
+# Installing ZAProxy
+curl https://github.com/zaproxy/zaproxy/releases/download/2.7.0/ZAP_2_7_0_unix.sh && \
+  chmod +x ZAP_2_7_0_unix.sh && ./ZAP_2_7_0_unix.sh
