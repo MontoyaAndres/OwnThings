3068f8736b1fea69c0d742a5c2b01aecc0551893
Andrés Montoya <andresmontoyafcb@gmail.com>
2019-05-20 21:55:32 -0500

    Update tools.sh

--- tools/tools.sh
+++ tools/tools.sh
@@ -2,16 +2,21 @@
 # You need to have install git
 # giving permissions with "chmod +x tools.sh"
 
+# Main dependencies
+
+sudo apt install apt-transport-https curl ca-certificates software-properties-common build-essential libssl-dev nmap python3 python3-pip tor qbittorrent gparted skypeforlinux k3d -y
+
+# Python tools
+
+sudo apt install python3-tk
+pip3 install setuptools
+
 # Installing vscode
-sudo apt install curl -y
 sudo curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt install apt-transport-https -y
 sudo apt update
 sudo apt install code -y
-
-sleep 10
 
 # Installing vscode extensions.
 
@@ -39,22 +44,14 @@ code --install-extension vscode-icons-team.vscode-icons
 # Adding favorite config for vscode
 cp settings.json ~/.config/Code/User/settings.json
 
-sleep 20
-
 # Installing databases
 sudo apt update
 sudo apt install mysql-server postgresql postgresql-contrib redis-server
 
 # Installing Docker
-sudo apt update
-sudo apt install apt-transport-https ca-certificates software-properties-common -y
-
-sleep 10
 
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
-
-sleep 10
 
 if [ "$(. /etc/os-release; echo $NAME)" = "Linux Mint" ] || [ "$(. /etc/os-release; echo  $NAME)" = "elementary OS" ] ; then
   # Linux mint 19 version or elementary os version
@@ -74,35 +71,24 @@ sudo apt install docker-ce -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-# Installing nodejs
-sudo curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
-sudo apt install -y nodejs
+# Installing nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
 # Installing yarn
 sudo curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 sudo echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update
-sudo apt install yarn -y
-
-# Installing some awesome tools
-sudo apt update
-sudo apt install nmap python3 python3-pip tor qbittorrent gparted skypeforlinux k3d -y
-
-# Installing python3 stuff
-
-sudo apt install python3-tk
-pip3 install setuptools
+sudo apt instal
 
 # Installing spotify
 sudo chmod +x spotify.sh
 sudo ./spotify.sh
 
 # Installing favorite npm packages.
-sudo npm config set unsafe-perm true
 sudo chown -R $USER:$(id -gn $USER) ~/.config
-sudo npm i -g eslint now tslint typescript npx
+sudo npm i -g eslint tslint typescript npx
 
-## React-native
+## React-native modules
 sudo apt install openjdk-8-jdk openjdk-8-jre -y
 sudo apt update
 sudo apt install libtool m4 automake pkg-config libssl-dev adb python python3 python-dev python3-dev gcc g++ make -y
@@ -114,10 +100,6 @@ sudo ./autogen.sh
 sudo ./configure
 sudo make
 sudo make install
-
-sleep 30
-
-sudo npm install -g expo-cli
 
 sudo apt clean \
 && sudo apt autoclean \
