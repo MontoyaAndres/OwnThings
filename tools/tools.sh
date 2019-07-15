2b010856b4f426e3922cffcdf0fa22db781e6564
Andrés Montoya <andresmontoyafcb@gmail.com>
2019-07-15 11:40:25 -0500

    Update tools.sh

--- tools/tools.sh
+++ tools/tools.sh
@@ -46,7 +46,7 @@ sudo apt install postgresql postgresql-contrib redis-server mongodb-server
 # Installing docker
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
 sudo apt install docker-ce -y
 
