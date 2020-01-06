42eea1e784bcc466baa639203349ffb42b464993
Andrés Montoya <andresmontoyafcb@gmail.com>
2020-01-05 20:58:36 -0500

    Update tools.sh

--- tools/tools.sh
+++ tools/tools.sh
@@ -37,4 +37,4 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 sudo apt install node-pre-gyp build-essential gcc g++ make
 nvm install node
-npm i tslint typescript yarn now -g --unsafe-perm
+npm i tslint typescript yarn now netlify-cli -g --unsafe-perm
