91ce37c048b9ab5e23db3901a898deb9631c72f7
Andrés Montoya <andresmontoyafcb@gmail.com>
2021-01-23 16:46:46 -0500

    Update tools.sh

--- tools/tools.sh
+++ tools/tools.sh
@@ -1,31 +1,16 @@
 # Main dependencies
-sudo apt install vlc curl nmap qbittorrent gparted ssh -y
+sudo apt install vlc curl nmap qbittorrent gparted ssh python3-pip -y
+sudo pip3 install cfn-lint
 https://marcosmarti.org/como-eliminar-el-ruido-de-fondo-de-un-microfono-en-linux/
 
 # Installing vscode
 https://code.visualstudio.com/docs/setup/linux#_debian-and-ubuntu-based-distributions
 
-# Installing vscode extensions.
-## Bracket Pair Colorizar
-code --install-extension CoenraadS.bracket-pair-colorizer
-## GitLens
-code --install-extension eamodio.gitlens
-## Prettier - Code formatter
-code --install-extension esbenp.prettier-vscode
-## TSLint
-code --install-extension ms-vscode.vscode-typescript-tslint-plugin
-## VSCode-icons
-code --install-extension vscode-icons-team.vscode-icons
-## AWS Toolkit
-code --install-extension amazonwebservices.aws-toolkit-vscode
-## vscode-styled-components
-code --install-extension jpoissonnier.vscode-styled-components
-
 # Adding favorite config for vscode
 cp settings.json ~/.config/Code/User/settings.json
 
 # Installing databases
-# sudo apt install postgresql postgresql-contrib redis-server mongodb-server
+sudo apt install mariadb-server
 
 # Installing nodejs
 https://github.com/nvm-sh/nvm#install--update-script
